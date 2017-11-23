4c8e059f9a2ab52b348c38daf6d1f92fa24c2a87
Jason Seminara <jseminara@gmail.com>
2017-11-23 12:29:33 -0500

    updated hash

--- weather.rb
+++ weather.rb
@@ -2,8 +2,8 @@ class Weather < Formula
   desc "The only command-line tool to retrieve local weather"
   homepage "https://github.com/jasonseminara/weather"
   url "https://github.com/jasonseminara/weather/blob/master/archive/weather-1.0.0.tar.gz"
-  version "1.0.0"
-  sha256 "67d335cccca65d650165f4e9d3be46cb31738a6dc622e4ed3540b3e05bfb94e9"
+  version "1.0.1"
+  sha256 "2d877400850ba4524709e6f23366edf3965df1cb69c08d0fe315c0700c11bcf5"
 
   depends_on "curl"
   depends_on "tig"

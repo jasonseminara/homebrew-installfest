286a4a625859baeea1a6539275acac5ae475c625
Jason Seminara <jseminara@gmail.com>
2017-11-24 14:56:44 -0500

    tarball pointing to correct location

--- installfest.rb
+++ installfest.rb
@@ -1,7 +1,7 @@
 class Installfest < Formula
   desc "The premiere installer for WDI NYC"
   homepage "https://git.generalassemb.ly/wdi-nyc/installfest"
-  url "https://git.generalassemb.ly/wdi-nyc/installfest/blob/homebrew/installfest-2.0.0.tar.gz"
+  url "https://git.generalassemb.ly/wdi-nyc/installfest/raw/homebrew/installfest-2.0.0.tar.gz"
   version "2"
   sha256 "25a28dcf3662bb70dc2120749777370dd451370882f6216901c3b1185e7c1704"
 

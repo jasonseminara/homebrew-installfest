412571d3d819431d918c059686d77a33951455c7
{{user_full_name}} <{{user_email}}>
2017-12-24 00:17:34 -0500

    rvm and admin rights

--- installfest.rb
+++ installfest.rb
@@ -5,53 +5,78 @@ class Installfest < Formula
   version "2"
   sha256 "6e033713d93dce57ab80207b3c9a9076e37d6b4339bc7f476ae2b6b2af3529f4"
 
-
   bottle :unneeded
 
-  depends_on "git"
-
-
   def install
-    ohai "#{Tty.blue}Welcome to the Installfest!"
+    ohai "#{Tty.blue}Welcome to WDI Installfest!"
 
     #######
     ### pre-flight
 
     # check for admin
-    ohai "Pre-Flight..."
-    ohai "#{Tty.green}Checking for admin privileges..."
-    if !system "id -Gn $whoami | grep -qw admin"
-      onoe <<~EOS
-        #{Tty.red}#{Tty.bold}
-        The current user does not have administrator privileges.
-        #{Tty.reset}You must run this program from an admin user.
-        Ask an instructor for help.
-        Exiting..."
-      EOS
-      raise SecurityError
-    end
+    #ohai "Checking user..."
+    #print "\t#{Tty.green}Checking for admin privileges..."
+    #if quiet_system "id -Gn $whoami | grep -qw admin"
+    #  raise onoe <<~EOS
+    #    #{Tty.bold}
+    #    The current user does not have administrator privileges.
+    #      #{Tty.reset}You must run this program from an admin user.
+    #      Ask an instructor for help.
+    #      Exiting..."
+    #  EOS
+    #end
+    #print "\t#{Tty.green}done!\n"
 
 
     # check for mixed-case username
-    ohai "#{Tty.green}Checking for mixed-case..."
+    print "\t#{Tty.green}Checking for mixed-case..."
     if ENV['LOGNAME'][/[A-Z]/]
-      raise SecurityError, 'mixed-case'
+      raise onoe <<~EOS
+        #{Tty.bold}
+        Your username #{ENV['LOGNAME']} cannot be 'mixed-case'; it should be entirely in lowercase.
+          #{Tty.reset}This could lead to some issues where certain tools that are
+          case-sensitive, and others that are not, don't work well together.
+          It is suggested that you change your username.
+          PS: this also goes for your GitHub username!
+      EOS
     end
+    ohai "\t#{Tty.green}done!"
 
     # check for spaces in username
-    ohai "#{Tty.green}Checking for spaces..."
+    print "\t#{Tty.green}Checking for spaces in your username..."
     if ENV['LOGNAME'][/\s/]
-      raise SecurityError, 'poop'
+      raise onoe <<~EOS
+        #{Tty.bold}
+        Your username #{ENV['LOGNAME']} has a space.
+          This complicates using command line tools, and can even break some programs.
+          Change your username before continuing.                              "
+          Exiting...
+      EOS
     end
+    ohai "\t#{Tty.green}done!"
 
     # check for command-line tools
-    if MacOS::CLT.outdated?
-      raise StandardError, "xcode"
-    end
+    ohai "#{Tty.green}Checking for XCode Command Line Tools..."
+    system "xcode-select", "--install" if MacOS::CLT.outdated?
+    ohai "XCode"
+
+    # software updates
+    ohai "#{Tty.green}Running software update on Mac OS..."
+    # system "softwareupdate", "-ir"
+    ohai "#{Tty.green}Software updated!"
+
+    # ensure user owns their home folder
+    ohai "#{Tty.green}Ensuring the current user owns their home folder..."
+    # system "chown", "-R", ENV['LOGNAME'], "/Users/#{ENV['LOGNAME']}"
+    ohai "#{Tty.green}Home directory owned!"
 
     ### pre-install
-    # software updates
     # remove rvm
+    ohai "#{Tty.green}Removing RVM..."
+    if quiet_system "hash rvm"
+      ohai "rvm installed"
+    end
+    ohai "#{Tty.green}rvm removed!"
     # remove macports
 
     ### install
